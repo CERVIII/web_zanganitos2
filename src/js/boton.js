0883bbb8c5fa133a43157456213349b5779a05b6
Pablo Cervilla <pablo.cervilla117@gmail.com>
2023-10-30 10:22:56 +0100

    boton filters

--- src/js/boton.js
+++ src/js/boton.js
@@ -1,7 +1,17 @@
+
+/* Boton Menu Header */
 const boton = document.querySelector('#boton')
 const menu = document.querySelector('#menu')
 
 boton.addEventListener('click', () => {
 	console.log('me diste clic')
 	menu.classList.toggle('hidden')
+})
+
+//const boton = document.querySelector('#boton')
+const filters = document.querySelector('#filters')
+
+boton.addEventListener('click', () => {
+	console.log('me diste clic')
+	filters.classList.toggle('hidden')
 })
